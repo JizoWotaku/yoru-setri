8eb5efb984cf13a2dd0708d193cbd101f5fa3b73
JizoWotaku <yabaiwebyasan+jizowotaku@gmail.com>
2026-03-08 17:52:19 +0900

    Add a new song

--- src/constants.ts
+++ src/constants.ts
@@ -7,6 +7,7 @@ export const SONG_LIST: Song[] = [
   { title: "SE", keywords: "seえすいー" },
   { title: "MC", keywords: "mcえむしー" },
   { title: "アンコール", keywords: "encoreあんこーる" },
+  { title: "Delight", keywords: "でらいとデライトDelight"},
   {
     title: "あたしイズム宣言!!!",
     keywords:
